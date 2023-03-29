bedf372b13fdcad1e0a9c31a520d229f933aefc0
shusei-kagari <kagari.shusei@proton.me>
2023-03-28 19:32:07 -0500

    feat: add docs.tsx deps and docs.tsx compiles now

--- lib/docs.tsx
+++ lib/docs.tsx
@@ -6,80 +6,87 @@ import html from 'remark-html';
 
 
 const postsDirectory = path.join(process.cwd(), 'documentation/guides');
-
-export function getSortedPostsData() {
-  // Get file names under /posts
-  const fileNames = fs.readdirSync(postsDirectory);
-  const allPostsData = fileNames.map((fileName) => {
-    // Remove ".md" from file name to get id
-    const id = fileName.replace(/\.md$/, '');
-
-    // Read markdown file as string
-    const fullPath = path.join(postsDirectory, fileName);
-    const fileContents = fs.readFileSync(fullPath, 'utf8');
-
-    // Use gray-matter to parse the post metadata section
-    const matterResult = matter(fileContents);
-
-    // Combine the data with the id
-    return {
-      id,
-      ...matterResult.data,
-    };
-  });
-  // Sort posts by date
-  return allPostsData.sort((a, b) => {
-    if (a.date < b.date) {
-      return 1;
-    } else {
-      return -1;
-    }
-  });
+interface PostData {
+	id: string;
+	date: string;
+	title: string;
+	[key: string]: any;
 }
 
-export function getAllPostIds() {
-  const fileNames = fs.readdirSync(postsDirectory);
+export function getSortedPostsData(): PostData[] {
+	// Get file names under /posts
+	const fileNames = fs.readdirSync(postsDirectory);
+	const allPostsData: PostData[] = fileNames.map((fileName) => {
+		// Remove ".md" from file name to get id
+		const id = fileName.replace(/\.md$/, '');
 
-  // Returns an array that looks like this:
-  // [
-  //   {
-  //     params: {
-  //       id: 'ssg-ssr'
-  //     }
-  //   },
-  //   {
-  //     params: {
-  //       id: 'pre-rendering'
-  //     }
-  //   }
-  // ]
-  return fileNames.map((fileName) => {
-    return {
-      params: {
-        id: fileName.replace(/\.md$/, ''),
-      },
-    };
-  });
+		// Read markdown file as string
+		const fullPath = path.join(postsDirectory, fileName);
+		const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+		// Use gray-matter to parse the post metadata section
+		const matterResult = matter(fileContents);
+
+		// Combine the data with the id
+		return {
+			id,
+			...matterResult.data,
+		} as PostData; // Asserting that the resulting object has the PostData type
+	});
+
+	// Sort posts by date
+	return allPostsData.sort((a, b) => {
+		if (a.date < b.date) {
+			return 1;
+		} else {
+			return -1;
+		}
+	});
 }
 
-export async function getPostData(id) {
-  const fullPath = path.join(postsDirectory, `${id}.md`);
-  const fileContents = fs.readFileSync(fullPath, 'utf8');
 
-  // Use gray-matter to parse the post metadata section
-  const matterResult = matter(fileContents);
+export function getAllPostIds(): { params: { id: string } }[] {
+	const fileNames = fs.readdirSync(postsDirectory);
 
-  // Use remark to convert markdown into HTML string
-  const processedContent = await remark()
-    .use(html)
-    .process(matterResult.content);
-  const contentHtml = processedContent.toString();
-
-  // Combine the data with the id and contentHtml
-  return {
-    id,
-    contentHtml,
-    ...matterResult.data,
-  };
+	// Returns an array that looks like this:
+	// [
+	//   {
+	//     params: {
+	//       id: 'ssg-ssr'
+	//     }
+	//   },
+	//   {
+	//     params: {
+	//       id: 'pre-rendering'
+	//     }
+	//   }
+	// ]
+	return fileNames.map((fileName) => {
+		return {
+			params: {
+				id: fileName.replace(/\.md$/, ''),
+			},
+		};
+	});
 }
 
+export async function getPostData(id: string): Promise<PostData & { contentHtml: string }> {
+	const fullPath = path.join(postsDirectory, `${id}.md`);
+	const fileContents = fs.readFileSync(fullPath, 'utf8');
+
+	// Use gray-matter to parse the post metadata section
+	const matterResult = matter(fileContents);
+
+	// Use remark to convert markdown into HTML string
+	const processedContent = await remark()
+		.use(html)
+		.process(matterResult.content);
+	const contentHtml = processedContent.toString();
+
+	// Combine the data with the id and contentHtml
+	return {
+		id,
+		contentHtml,
+		...matterResult.data,
+	} as PostData & { contentHtml: string };
+}
