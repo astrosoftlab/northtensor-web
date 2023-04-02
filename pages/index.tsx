2189a8ccaf10e6ea15a2ee52b392850a5a20f8f5
madofuller <madofuller@gmail.com>
2023-04-02 12:09:14 -0600

    updated hero and cards

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,6 @@ import Head from 'next/head'
 import Image from 'next/image'
 import Link from 'next/link'
 import { Inter } from 'next/font/google'
-import styles from '@/styles/Home.module.css'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -16,46 +15,46 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      <main className="flex flex-col justify-between items-center py-24 min-h-screen">
-        <div className={styles.hero}>
+      <main>
+        <div className="relative bg-[url('../public/images/wallpaperflare.com_wallpaper.jpg')] px-4 py-32 sm:px-6 lg:flex  lg:items-center ">
         <div className="w-4/5 mx-auto mb-8">
-          <div className="mt-4 mb-4 transition-opacity duration-200 text-6xl font-serif leading-20 uppercase">
+          <div className="text-3xl font-extrabold sm:text-5xl text-gray-100">
             MNRV.AI
           </div>
           <br />
-          <p className="mb-4 opacity-100 text-lg leading-8 font-normal">
+          <p className="mb-4 opacity-100 text-lg leading-8 font-normal text-gray-100">
             We are a Bittensor-first AI Firm aiming to utilize the inevitable union of economic efficiencies provided through cryptocurrency technologies. Our focus is to create infrastructure required to scale and support basic transformer driven data extraction to Artificial General Intelligence.
           </p>
-          <p className="mb-4 opacity-100 text-lg leading-8 font-normal">
+          <p className="mb-4 opacity-100 text-lg leading-8 font-normal text-gray-100">
             Check out our introductory guides
           </p>
-          <Link href="/docs/introduction-to-bittensor" className={[styles.button, styles.cc_jumbo_button, styles.cc_jumbo_white, styles.w_inline_block].join(" ")} >
+          <Link href="/docs/introduction-to-bittensor" className="leading-relaxed whitespace-nowrap block w-1/6 h-11 rounded tracking-wide text-2xl bg-white px-10 text-gray-800 shadow active:text-gray-500 " >
           <div>Getting Started</div>
           </Link>
           <br />
-          <p className="mb-4 opacity-100 text-lg leading-8 font-normal">
+          <p className="mb-4 opacity-100 text-lg leading-8 font-normal text-gray-100">
             Or, just start Staking
           </p>
-          <Link href="https://app.mnrv.ai" className={[styles.button, styles.cc_jumbo_button, styles.cc_jumbo_white, styles.w_inline_block].join(" ")} >
+          <Link href="https://app.mnrv.ai" className="leading-relaxed block w-1/6 h-11 rounded tracking-wide text-2xl bg-white px-16 text-gray-800 shadow active:text-gray-500 ">
           <div>Stake Tao</div>
           </ Link>
         </div>
         </div>
 
-        <div className="grid grid-cols-1 mb-24 max-w-xs mx-auto text-center">
+        <div className="container mx-auto flex flex-wrap items-start my-16">
           <Link
             href="/docs/introduction-to-bittensor"
-            className={styles.card}
+            className="bg-gray-200 rounded-xl p-6 lg:w-1/5 w-full lg:pr-3 container mx-auto flex flex-wrap items-start my-16"
             target="_self"
             rel="noopener noreferrer">
-            <h2 className={inter.className}>
+            <h2 className="text-2xl font-bold mb-2">
               Developers <span>-&gt;</span>
             </h2>
             <p className="tagline italic font-bold">
               A Bittensor backend is just the start for building AI solutions.
             </p>
             <br />
-            <p className={inter.className}>
+            <p className="text-gray-800 leading-relaxed">
               We&apos;re hoping to help bring the builder culture back into crypto, where collaboration and innovating killer apps that truly disrupt the world as we know it thrive.
             <br />
             <br />
@@ -64,17 +63,17 @@ export default function Home() {
             </ Link>
           <Link
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="bg-gray-200 rounded-xl p-6 lg:w-1/4 w-full lg:pr-3 container mx-auto flex flex-wrap items-start my-16"
             target="_blank"
             rel="noopener noreferrer" >
-            <h2 className={inter.className}>
+            <h2 className="text-2xl font-bold mb-2">
               Users <span>-&gt;</span>
             </h2>
             <p className="tagline italic font-bold">
               AI can be used for more than you think.
             </p>
             <br />
-            <p className={inter.className}>
+            <p className="text-gray-800 leading-relaxed">
               We&apos;re investing in further development and implementation of AI to inspire simple solutions to complex problems. 
             <br />
             <br />
@@ -84,18 +83,18 @@ export default function Home() {
 
           <Link
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="bg-gray-200 rounded-xl p-6 lg:w-1/4 w-full lg:pr-3 container mx-auto flex flex-wrap items-start my-16"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 className="text-2xl font-bold mb-2">
               Stakers <span>-&gt;</span>
             </h2>
             <p className="tagline italic font-bold">
               Your Tao can do good for both you and the rest of the world.
             </p>
             <br />
-            <p className={inter.className}>
+            <p className="text-gray-800 leading-relaxed">
               With experience validating since early 2022, we appreciate how valuable validating is to the future of Bittensor. We want to scale that value out.
             <br />
             <br />
@@ -108,18 +107,18 @@ export default function Home() {
 
           <a
             href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
+            className="bg-gray-200 rounded-xl p-6 lg:w-1/4 w-full lg:pr-3 container mx-auto flex flex-wrap items-start my-16"
             target="_blank"
             rel="noopener noreferrer"
           >
-            <h2 className={inter.className}>
+            <h2 className="text-2xl font-bold mb-2">
               Miners <span>-&gt;</span>
             </h2>
-            <p className={styles.tagline}>
+            <p className="tagline italic font-bold">
               You can help mine, no matter your skill level.
             </p>
             <br />
-            <p className={inter.className}>
+            <p className="text-gray-800 leading-relaxed">
               We personally know the complexity and expenses of mining Tao. We want to break down those barriers to allow you to contribute to mining no matter your skill level, and receive Tao inflation for it. 
             <br />
             <br />
