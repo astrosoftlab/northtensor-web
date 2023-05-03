b42ca694a712e82e9c769e8168ff6a8237531ecd
Matthew <madofuller@gmail.com>
2023-05-03 12:35:33 -0600

    fixed header in navbar

--- components/tailwindui/Navbar.tsx
+++ components/tailwindui/Navbar.tsx
@@ -1,19 +1,27 @@
+import Image from "next/image";
 import { Dialog } from '@headlessui/react'
 import { Bars3Icon, XMarkIcon } from '@heroicons/react/24/outline'
+import { useSession, useSupabaseClient } from '@supabase/auth-helpers-react'
+import { useState } from 'react';
 
 const navigation = [
+  { name: 'Resources', href: '#' },
+  { name: 'Projects', href: '#' },
+  { name: 'Roadmap', href: '#' },
   { name: 'Company', href: '#' },
 ]
 
 export default function Example() {
   const [mobileMenuOpen, setMobileMenuOpen] = useState(false)
+  const session = useSession()
+  const supabase = useSupabaseClient()
   return (
     <header className="bg-gray-900">
       <nav className="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
         <div className="flex lg:flex-1">
           <a href="/" className="-m-1.5 p-1.5">
-            <span className="sr-only">North Tensor</span>
-            <img className="h-8 w-auto" src="/images/newlogo.svg" alt="" />
+            <span className="sr-only">Your Company</span>
+            <img className="h-8 w-auto" src="./images/invert.svg" alt="" />
           </a>
         </div>
         <div className="flex lg:hidden">
@@ -33,14 +41,32 @@ export default function Example() {
             </a>
           ))}
         </div>
+        <div className="flex flex-1 items-center justify-end gap-x-6">
+          {!session ? (
+            <a href="/profile/" className="ml-auto rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
+              Log In
+            </a>
+          ) : (
+            <div className="flex flex-1 items-center justify-end gap-x-6">
+              <a href="/profile/" className="ml-auto rounded-md bg-slate-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-slate-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-slate-600">
+                Account 
+              </a>
+              <button type="button" className="text-sm font-semibold leading-6 text-slate-100 ml-2" onClick={() => supabase.auth.signOut()}>
+                Log Out
+              </button>
+            </div>
+          )}
         </div>
+
+
+
       </nav>
       <Dialog as="div" className="lg:hidden" open={mobileMenuOpen} onClose={setMobileMenuOpen}>
         <div className="fixed inset-0 z-10" />
         <Dialog.Panel className="fixed inset-y-0 right-0 z-10 w-full overflow-y-auto bg-gray-900 px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-white/10">
           <div className="flex items-center justify-between">
             <a href="#" className="-m-1.5 p-1.5">
-              <span className="sr-only">North Tensor</span>
+              <span className="sr-only">Your Company</span>
               <img
                 className="h-8 w-auto"
                 src="https://tailwindui.com/img/logos/mark.svg?color=slate&shade=500"
@@ -70,7 +96,9 @@ export default function Example() {
                 ))}
               </div>
               <div className="py-6">
+
                 <a
+                  href="/profile/"
                   className="-mx-3 block rounded-lg px-3 py-2.5 text-base font-semibold leading-7 text-white hover:bg-gray-800"
                 >
                   Log in
