eca4fc462ffe964448a2124d2a7894acec411ece
Bradleytf <2593536+bradleytf@users.noreply.github.com>
2023-05-03 15:17:18 -0500

    Add input box for coldkeys

---
+++ components/tailwindui/InputLabelled.jsx
@@ -0,0 +1,18 @@
+export default function Example() {
+  return (
+    <div className="isolate -space-y-px rounded-md shadow-sm">
+      <div className="relative rounded-md rounded-b-none px-3 pb-1.5 pt-2.5 ring-1 ring-inset ring-gray-300 focus-within:z-10 focus-within:ring-2 focus-within:ring-indigo-600">
+        <label htmlFor="name" className="block text-xs font-medium text-gray-900">
+          Name
+        </label>
+        <input
+          type="text"
+          name="name"
+          id="name"
+          className="block w-full border-0 p-0 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+          placeholder="Jane Smith"
+        />
+      </div>
+    </div>
+  )
+}
