9078b328008ea786acc1a750318aa88b35acc125
Bradleytf <2593536+bradleytf@users.noreply.github.com>
2023-05-14 00:43:12 -0500

    Fix profile page logo reference

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -14,10 +14,10 @@ export default function Home() {
         <div className="sm:mx-auto sm:w-full sm:max-w-sm">
           { !session &&
             <>
-              <Image
+              <img
                 className="mx-auto h-16 w-auto"
                 src="/images/newlogo.svg"
-                alt="Your Company"
+                alt="North Tensor"
               />
               <h2 className="mt-10 text-center text-2xl font-bold leading-9 tracking-tight text-slate-900">
                 Sign in to your account
