0a12e2a9fed0f0d8ef828388a8cdbd82792499c2
Jason Mendoza <mernstackdev729@gmail.com>
2023-08-17 13:34:46 -0400

    fix: removed deprecated supabase createBrowserSupabaseClient

--- src/pages/_app.page.tsx
+++ src/pages/_app.page.tsx
@@ -4,13 +4,11 @@ import type { AppProps } from "next/app"
 import { Inter } from "next/font/google"
 import Head from "next/head"
 
-import { createBrowserSupabaseClient } from "@supabase/auth-helpers-nextjs"
+import { createPagesBrowserClient } from "@supabase/auth-helpers-nextjs"
 import { Session, SessionContextProvider } from "@supabase/auth-helpers-react"
 
 import Layout from "@components/layout"
 import "@styles/globals.css"
-
-// import landing from "@/tailwindui/Landing.tsx"
 
 const inter = Inter({
   subsets: ["latin"],
@@ -23,7 +21,7 @@ export default function App({
 }: AppProps<{
   initialSession: Session
 }>) {
-  const [supabase] = useState(() => createBrowserSupabaseClient())
+  const [supabase] = useState(() => createPagesBrowserClient())
 
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
