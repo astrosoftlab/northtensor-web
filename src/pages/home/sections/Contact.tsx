1edb466cc831a7fe352d8e3a436e502dc5de96c3
Jason Mendoza <mernstackdev729@gmail.com>
2023-09-13 21:31:51 -0400

    fix: removed contact us form temporily

--- src/pages/home/sections/Contact.tsx
+++ src/pages/home/sections/Contact.tsx
@@ -16,7 +16,7 @@ export const ContactSection = () => {
             enhancements to the network, and strive to support them in their endeavors.
           </div>
         </div>
-        <div className="flex flex-col items-center lg:gap-[50px] gap-[36px]">
+        {/* <div className="flex flex-col items-center lg:gap-[50px] gap-[36px]">
           <div className="max-w-[960px] w-full rounded-lg p-[1px] bg-[linear-gradient(0deg,rgba(159,111,203,1)0%,rgba(107,56,152,1)50%,rgba(62,29,96,1)100%)]">
             <div className="flex justify-center lg:p-[48px] p-[36px] rounded-[7px] bg-[#060606]">
               <div className="max-w-[556px] w-full">
@@ -31,7 +31,7 @@ export const ContactSection = () => {
               </div>
             </div>
           </div>
-        </div>
+        </div> */}
       </div>
       <div className="h-[350px]" style={{ background: `url(${tensorGradian.src}) no-repeat center bottom` }}></div>
     </section>
